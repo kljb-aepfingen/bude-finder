d5f65b35fd37479b812df17a1d923bcf7c63fa49
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-12-30 17:19:52 +0100

    made markers clickable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next"
 import Head from "next/head"
-import {useEffect} from 'react'
+import {useEffect, useState, useRef, useCallback} from 'react'
 
 import {useMap} from '@/utils/map'
 import {trpc} from '@/utils/trpc'
@@ -8,17 +8,28 @@ import {trpc} from '@/utils/trpc'
 const Home: NextPage = () => {
   const {map} = useMap()
   const {data} = trpc.bude.all.useQuery()
+  const [info, setInfo] = useState<{name: string, description: string} | null>(null)
 
   useEffect(() => {
     if (!data)
       return
 
-    const markers = data.map(({lat, lng, name}) => {
+    const markers = data.map(({lat, lng, name, description}) => {
       const marker = new google.maps.Marker({map, position: {lat, lng}, title: name})
+      google.maps.event.addListener(marker, 'click', () => {
+        setInfo({name, description})
+      })
       return marker
     })
 
-    return () => markers.forEach(marker => marker.setMap(null))
+    const listener = map.addListener('click', () => {
+      setInfo(null)
+    })
+
+    return () => {
+      markers.forEach(marker => marker.setMap(null))
+      google.maps.event.removeListener(listener)
+    }
   }, [data, map])
 
   return <>
@@ -26,6 +37,7 @@ const Home: NextPage = () => {
       <title>Bude Finder</title>
     </Head>
     <Account/>
+    <Info info={info}/>
   </>
 }
 
@@ -51,4 +63,47 @@ const Account = () => {
   return <button onClick={() => signIn('google')} className={`${classNames} p-3`}>
     <SignUpSVG/>
   </button>
+}
+
+
+interface InfoProps {
+  info: null | {
+    name: string,
+    description: string
+  }
+}
+
+const Info = ({info}: InfoProps) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const prevoius = useRef<InfoProps['info'] | null>(null)
+
+  useEffect(() => {
+    if (!ref.current)
+      return
+    const div = ref.current
+    if (info) {
+      prevoius.current = info
+      div.style.display = 'block'
+      div.style.height = 'auto'
+      const {height} = div.getBoundingClientRect()
+      div.style.height = '0px'
+      div.getBoundingClientRect()
+      div.style.height = `${height}px`
+      return
+    }
+    div.style.height = '0px'
+  }, [info])
+
+  const handleTransitionEnd = useCallback(() => {
+    if (ref.current && !info) {
+      ref.current.style.display = 'none'
+    }
+  }, [info])
+
+  return <div onTransitionEnd={handleTransitionEnd} ref={ref} className="overflow-hidden fixed bottom-0 left-0 right-0 bg-slate-800 transition-[height] hidden">
+    <div className="grid grid-cols-1 p-4 gap-4">
+      <h1 className="text-4xl">{prevoius.current?.name}</h1>
+      <div className="text-lg">{prevoius.current?.description}</div>
+    </div>
+  </div>
 }
