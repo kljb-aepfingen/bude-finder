fc14a4bf1a39109edb79e00aa413f298823139cc
pedeEli <74102959+pedeEli@users.noreply.github.com>
2023-01-04 13:42:13 +0100

    added google adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -102,6 +102,17 @@ const MyApp: AppType<{ session: Session | null }> = ({
     <SessionProvider session={session}>
       <Wrapper apiKey={env.NEXT_PUBLIC_MAPS_KEY}>
         <div className="flex flex-col h-full">
+          <ins
+            className="adsbygoogle"
+            style={{display: 'block'}}
+            data-ad-client="ca-pub-8241943697814118"
+            data-ad-slot="7494837909"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
           <div ref={ref} className="flex-1"/>
           {map && <mapContext.Provider value={{map}}>
             <BudeProvider>
