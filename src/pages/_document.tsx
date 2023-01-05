38dfc85e97771cd67c57a7d470605ce2fdf4c35d
pedeEli <74102959+pedeEli@users.noreply.github.com>
2023-01-05 19:16:15 +0100

    removed google adsense for now

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,6 @@ const Document = () => {
     <Head>
       <meta name="description" content="Findet buden in deiner Umgebung" />
       <link rel="icon" href="/favicon.ico" />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8241943697814118" crossOrigin="anonymous"></script>
-      <script async defer src="/temp.js"></script>
     </Head>
     <body>
       <Main/>
