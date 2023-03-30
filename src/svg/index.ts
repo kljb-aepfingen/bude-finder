be41264d967c0c40cf428c96d5791037a782be82
pedeEli <74102959+pedeEli@users.noreply.github.com>
2023-03-30 14:24:23 +0200

    bug fix: it help if you save the files you changed in the previous commit

--- src/svg/index.ts
+++ src/svg/index.ts
@@ -1,8 +1,5 @@
 import LeftSVG from './Left'
 import RightSVG from './Right'
-import DownSVG from './Down'
-import SignUpSVG from './SignUp'
-import InfoSVG from './Info'
 import SpinnerSVG from './Spinner'
 import ThumbUpSVG from './ThumbUp'
 import ThumbDownSVG from './ThumbDown'
@@ -10,9 +7,6 @@ import ThumbDownSVG from './ThumbDown'
 export {
   LeftSVG,
   RightSVG,
-  DownSVG,
-  SignUpSVG,
-  InfoSVG,
   SpinnerSVG,
   ThumbUpSVG,
   ThumbDownSVG
