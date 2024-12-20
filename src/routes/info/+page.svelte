80d3074aa1a1ffea8d538032d2fa391af47925b5
Elias Gerster <74102959+pedeEli@users.noreply.github.com>
2024-12-20 14:33:46 +0100

    merged dev

---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,45 @@
+<div class="flex flex-col h-dfull">
+	<main class="flex flex-col flex-1 p-4 overflow-auto scroll-p-4">
+		<h1 class="text-2xl">Info</h1>
+		<div class="h-px bg-slate-600 my-4">&nbsp;</div>
+		<ol class="counter-dec">
+			<li>
+				<h2 id="warum">Warum diese App?</h2>
+				<p>
+					Wir haben das Problem, dass wir bei Flyertouren nie wissen, wo sich Buden und Landjugenden
+					befinden. Deshalb haben wir wir diese Webseite entwickelt und sind fleißig dabei alle
+					Buden und Landjugenden einzutragen. Falls eure Bude oder Landjugend noch nicht eingetragen
+					ist dann schreibt uns gern bei <a
+						href="https://www.instagram.com/kljb_aepfingen/"
+						target="_blank"
+						rel="noopener noreferrer"
+						class="underline text-sky-400">Instagram</a
+					>
+					oder meldet euch per
+					<a href="mailto:elias.gerster@outlook.de" class="underline text-sky-400">Mail</a>.
+				</p>
+			</li>
+			<li>
+				<h2 id="feedback">Feedback</h2>
+				<p>
+					Wenn ihr irgendwelche Fehler in unserer App entdeckt, egal ob Schreibfehler oder Bugs,
+					dann würde es uns freuen, wenn ihr euch meldet. Alternativ könnt ihr auch ein Issue auf
+					unserem <a
+						href="https://github.com/kljb-aepfingen/bude-finder"
+						class="underline text-sky-400"
+						target="_blank">Github</a
+					> erstellen.
+				</p>
+			</li>
+		</ol>
+	</main>
+
+	<div class="flex p-4 gap-8 justify-center">
+		<a href="/impressum" class="underline text-sky-400">Impressum</a>
+		<a href="/datenschutz" class="underline text-sky-400">Datenschutzerklärung</a>
+	</div>
+
+	<div class="flex border-t border-white">
+		<a class="p-4 flex-1" href="/">Zurück</a>
+	</div>
+</div>
